6496e2acccbeed325727b448cbb5edf42c83336c
KARTIKPAUL <arpilar401@gmail.com>
2024-06-28 13:28:59 +0530

    EsLint Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
       <>
         <h1>Hello</h1>
+        <Routes>
+          
+        </Routes>
       </>
   )
 }
